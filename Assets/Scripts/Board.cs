fd93f7846a693fcaf77ae78f0e55e780ad957790
ericphann <ephann@uncc.edu>
2023-05-02 21:35:50 -0400

    Add assets and game grid.
    Dots, grid, start swipe implementation

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    public GameObject[] dots;
+    private BackgroundTile[,] allTiles;
+    public GameObject[,] allDots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
+        Setup();
+    }
+
+    private void Setup() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0 ; j < height; j++) {
+
+                Vector2 tempPosition = new Vector2(i, j);
+
+                //create tile
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "( " + i + ", " + j + " )";
+
+                //create dot
+                int dotToUse = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + ", " + j + " )";
+                allDots[i, j] = dot;
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
